e71861bfe9a1a93e561be593e8e87d06c70adbfc
bodby <baraa.homsi@proton.me>
2024-12-02 13:41:49 +0300

    alpha: Hide statuscolumn

--- nvim/lua/bodby/native/statuscolumn.lua
+++ nvim/lua/bodby/native/statuscolumn.lua
@@ -2,6 +2,30 @@ local M = {}
 
 function M.setup()
   vim.opt.statuscolumn = "%!v:lua.require('bodby.native.statuscolumn').active()"
+
+  vim.api.nvim_create_autocmd({
+    "BufEnter",
+    "UIEnter"
+  }, {
+    group = "status",
+    callback = function()
+      vim.schedule(function()
+        if vim.bo.filetype == "alpha" then
+          require("bodby.native.statuscolumn").disable()
+        else
+          require("bodby.native.statuscolumn").enable()
+        end
+      end)
+    end
+  })
+end
+
+function M.enable()
+  vim.opt.statuscolumn = "%!v:lua.require('bodby.native.statuscolumn').active()"
+end
+
+function M.disable()
+  vim.opt.statuscolumn = "%!v:lua.require('bodby.native.statuscolumn').inactive()"
 end
 
 M.sign = function()
