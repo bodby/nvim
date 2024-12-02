59009b7e87c17b09a871d3be271e461969e44898
bodby <baraa.homsi@proton.me>
2024-12-02 16:07:36 +0300

    gitsigns: Added toggle for signs and deleted lines

--- nvim/lua/bodby/plugins/gitsigns-nvim.lua
+++ nvim/lua/bodby/plugins/gitsigns-nvim.lua
@@ -1,3 +1,5 @@
+local map = vim.keymap.set
+
 require("gitsigns").setup({
   signs = {
     add = { text = "│" },
@@ -15,11 +17,18 @@ require("gitsigns").setup({
     changedelete = { text = "│" },
     untracked = { text = "┆" },
   },
-  signs_staged_enable = true,
-  signcolumn = true,
+  signs_staged_enable = false,
+  signcolumn = false,
   numhl = false,
   linehl = false,
   word_diff = false,
   auto_attach = true,
-  update_debounce = 50,
+  update_debounce = 500
 })
+
+map("n", "<Leader>g", function()
+  require("gitsigns").toggle_deleted()
+  require("gitsigns").toggle_signs()
+  -- Fixes the deleted line sometimes not disappearing.
+  vim.cmd "redraw!"
+end)
