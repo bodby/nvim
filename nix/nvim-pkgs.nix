833a19bc8ee0b55d95f0318c6ac6cac926b14c4d
bodby <baraa.homsi@proton.me>
2024-12-02 12:00:00 +0300

    nvim-pkgs: Removed duplicate blink-cmp entry

--- nix/nvim-pkgs.nix
+++ nix/nvim-pkgs.nix
@@ -6,11 +6,11 @@
 }:
 
 let
-  # Custom plugins not available in nixpkgs.
+  # Custom plugins in nix/pkgs/.
   customPkgs = [
     pkgs.blink-indent
     pkgs.blink-compat
-    pkgs.fine-cmdline
+    # pkgs.fine-cmdline
     inputs.blink-cmp.packages.${system}.blink-cmp
   ];
 in
@@ -21,12 +21,11 @@ in
       nvim-treesitter.withAllGrammars
       nvim-treesitter-parsers.latex
       telescope-nvim
-      blink-cmp
+      # blink-cmp
       markview-nvim
       obsidian-nvim
       nvim-lspconfig
       nabla-nvim
-      smartcolumn-nvim
       telescope-zf-native-nvim
       alpha-nvim
       gitsigns-nvim
