cc9a2b78f0990d5fd7841d57efad8abf58bd8c59
bodby <baraa.homsi@proton.me>
2024-12-13 15:05:01 +0300

    winbar: Working window-specific bars

--- nvim/lua/bodby/native/winbar.lua
+++ nvim/lua/bodby/native/winbar.lua
@@ -1,25 +1,61 @@
 local M = {
   col = {
-    loc = "%#WinBarLOC#"
+    loc = "%#WinBarLOC#",
+    modified = "%#WinBarMod#"
   },
-  modified_char = "'"
+  modified_char = "'",
+  blocked_filetypes = {
+    "TelescopePrompt",
+    "blink-cmp-menu",
+    "blink-cmp-documentation",
+    "blink-cmp-signature"
+  }
 }
 
 function M.setup()
-  vim.opt.winbar = "%!v:lua.require('bodby.native.winbar').active()"
+  vim.wo.winbar = "%!v:lua.require('bodby.native.winbar').active(0)"
+
+  vim.api.nvim_create_autocmd({
+    "WinEnter",
+    "BufEnter"
+  }, {
+    group = "status",
+    callback = function(event)
+      vim.schedule(function()
+        local windows = vim.fn.win_findbuf(event.buf);
+        for _, window in pairs(windows) do
+          for _, ft in pairs(M.blocked_filetypes) do
+            if vim.bo[vim.api.nvim_win_get_buf(window)].filetype == ft then
+              return
+            end
+          end
+
+          vim.wo[window].winbar = "%!v:lua.require('bodby.native.winbar').active(" .. window .. ")"
+        end
+      end)
+    end
+  })
 end
 
-M.file = function()
-  return "%t"
+M.file = function(window)
+  local mod_suffix = ""
+  if vim.api.nvim_win_is_valid(window) then
+    if vim.bo[vim.api.nvim_win_get_buf(window)].modified then
+      mod_suffix = M.col.modified .. M.modified_char
+    end
+  end
+
+  return "%t" .. mod_suffix
 end
 
 M.loc = function()
   return M.col.loc .. "%L"
 end
 
-M.active = function()
+M.active = function(window)
   return table.concat({
-    " %t",
+    " ",
+    M.file(window),
     "%=",
     M.loc(),
     " "
