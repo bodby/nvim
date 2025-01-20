92af46496c1a5543721ee792e8f9eaeccdfef06c
Baraa Homsi <baraa.homsi@proton.me>
2025-01-20 16:03:36 +0000

    feat(TS): Shorter Nix function query and fixed attrset query

--- nvim/after/queries/nix/highlights.scm
+++ nvim/after/queries/nix/highlights.scm
@@ -1,30 +1,33 @@
 ;; extends
 
-; This took way too long.
+; TODO: PR this as this has been updated and is like 2 lines shorter.
 (apply_expression
-  function: [
-    (select_expression
-      attrpath: (attrpath
+  function: (select_expression
+    attrpath: [
+      (attrpath
         .
-        attr: (identifier) @function.call .))
-    (select_expression
-      attrpath: (attrpath
+        attr: (identifier) @function.call .)
+      (attrpath
         (_)
-        attr: (identifier) @function.call .))
-  ])
+        attr: (identifier) @function.call .)
+    ]))
 
 (variable_expression
   name: (identifier) @namespace.builtin
     (#any-of? @namespace.builtin "nixpkgs" "builtins" "lib" "pkgs"))
 
-; FIXME: Is "priority" needed?
-((binding
+; FIXME: Think there's an easier way to do this.
+(binding
   attrpath: (attrpath
     attr: (identifier) @variable)
   expression: (attrset_expression
     (binding_set
-      (binding
-        attrpath: (attrpath
-        attr: (identifier)))
-      (_)))
-  ))
+      binding: [
+        (binding
+          attrpath: (attrpath
+            attr: (identifier)))
+        (inherit
+          attrs: (inherited_attrs))
+      ]
+      (_)
+    )))
