0eb0a2a15c0e76a8d18178b729db1e81afc67dbc
Baraa Homsi <49784526+bodby@users.noreply.github.com>
2025-01-21 17:28:11 +0000

    TODO PLEASE

--- nvim/after/queries/nix/highlights.scm
+++ nvim/after/queries/nix/highlights.scm
@@ -1,16 +1,11 @@
 ;; extends
 
-; TODO: PR this.
+; FIXME: See if this works. See functions inside attrs inside attrs,
+; e.g. pkgs.hello.world.aaa { };
 (apply_expression
   function: (select_expression
-    attrpath: [
-      (attrpath
-        .
-        attr: (identifier) @function.call .)
-      (attrpath
-        (_)
-        attr: (identifier) @function.call .)
-    ]))
+    attrpath: (attrpath
+      attr: (identifier) @function.call .))
 
 (variable_expression
   name: (identifier) @module.builtin
