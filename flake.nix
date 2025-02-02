153a43bf8bf56fed626247ad3cc2beca30e29a5f
Baraa Homsi <baraa.homsi@proton.me>
2025-02-02 14:30:13 +0000

    misc(nix): Grammar

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style
       );
 
-      # TODO: Use mkNeovim function here instead of in package.nix to make customization easier.
+      # TODO: Use 'mkNeovim' here instead of in package.nix to make customization easier.
       packages = nixpkgs.lib.genAttrs systems (system:
         let
           pkgs = import nixpkgs {
