fcb6b520b00062f1ae1eb182763a85f9b59b005a
Baraa Homsi <baraa.homsi@proton.me>
2025-02-07 08:53:53 +0000

    feat(treesitter): Haskell queries

--- nvim/after/queries/haskell/highlights.scm
+++ nvim/after/queries/haskell/highlights.scm
@@ -13,6 +13,14 @@
 ;   ] @punctuation.bracket)
 
 ((signature)
-  .
   (bind
-    name: (variable) @function (#set! "priority" 101)))
+    name: (variable) @function)
+  (#set! "priority" 101))
+
+
+(infix_id
+  [
+    (variable) @function.call
+    (qualified
+      (variable) @function.call)
+  ])
