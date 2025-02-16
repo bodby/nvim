ce31f69ceb89da8a1b5d1f85a6b6f68d24a9bd0e
Baraa Homsi <baraa.homsi@proton.me>
2025-02-16 12:01:50 +0000

    feat(theme): Remove purple function keywords

--- nvim/colors/dark.lua
+++ nvim/colors/dark.lua
@@ -215,7 +215,7 @@ local hls = {
   ["@function.builtin"]    = { link = "Function" },
   ["@variable"]            = { fg = colors.white1 },
   ["@keyword.conditional"] = { link = "Conditional" },
-  ["@keyword.function"]    = { fg = colors.purple },
+  -- ["@keyword.function"]    = { fg = colors.purple },
   ["@punctuation.special"] = { link = "Operator" },
   ["@character.special"]   = { fg = colors.cyan },
   ["@constant.builtin"]    = { fg = colors.green },
