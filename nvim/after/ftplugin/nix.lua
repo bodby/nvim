c0f0cee49c0abbb2b8adda3c5a7759cc6ec14e6f
Baraa Homsi <baraa.homsi@proton.me>
2025-04-11 17:13:52 +0000

    feat(ftplugin): add Nix

--- nvim/after/ftplugin/nix.lua
+++ nvim/after/ftplugin/nix.lua
@@ -1,6 +1,6 @@
-vim.bo.commentstring = '/* %s */'
+vim.bo.commentstring = '# %s'
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
-vim.bo.textwidth = 80
+vim.bo.textwidth = 90
