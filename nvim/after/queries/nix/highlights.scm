e24e25299009aa43161d0bf88e299e1bccf45998
Baraa Homsi <baraa.homsi@proton.me>
2025-04-12 12:59:24 +0000

    misc(queries): delete unused comments

--- nvim/after/queries/nix/highlights.scm
+++ nvim/after/queries/nix/highlights.scm
@@ -1,35 +1,18 @@
 ;; extends
 
-function: (select_expression
-  attrpath: (attrpath
-    attr: (identifier) @function.call .))
-
 function: (_
   name: (identifier) @function.call)
+
+(binding
+  attrpath: (attrpath
+    attr: (identifier) @function)
+  expression: (function_expression))
 
 ; ((identifier) @module.builtin
 ;   (#any-of? @module.builtin "builtins" "lib" "pkgs"))
 
 ((identifier) @module.builtin
   (#eq? @module.builtin "builtins"))
-
-; (variable_expression
-;   name: (identifier) @module.builtin
-;     (#any-of? @module.builtin "builtins" "lib" "pkgs"))
-
-; (function_expression
-;   universal: (identifier) @module.builtin
-;     (#any-of? @module.builtin "builtins" "lib" "pkgs"))
-
-; (select_expression
-;   expression: (variable_expression)
-;   attrpath: (attrpath
-;     attr: (identifier) @module.builtin
-;       (#any-of? @module.builtin "builtins" "lib" "pkgs")))
-
-; (inherited_attrs
-;   attr: (identifier) @module.builtin
-;     (#any-of? @module.builtin "builtins" "lib" "pkgs"))
 
 (binding
   attrpath: (attrpath
@@ -50,11 +33,6 @@ function: (_
     binding: (binding
       attrpath: (attrpath
         attr: (identifier) @variable))))
-
-(binding
-  attrpath: (attrpath
-    attr: (identifier) @function)
-  expression: (function_expression))
 
 (inherit_from
   expression: (variable_expression
