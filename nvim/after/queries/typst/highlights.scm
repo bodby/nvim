d9ed4b05330e27d9ca3a54f7572928f79414fc9f
Baraa Homsi <baraa.homsi@proton.me>
2025-04-28 17:02:24 +0000

    feat(query): Typst queries

--- nvim/after/queries/typst/highlights.scm
+++ nvim/after/queries/typst/highlights.scm
@@ -1,3 +1,14 @@
 ;; extends
 
 (none) @constant.builtin
+
+(elude ".." @operator)
+
+(call
+  item: (field
+    (_)
+    field: (ident) @function .))
+
+(lambda
+  pattern: (ident)
+  "=>" @operator)
