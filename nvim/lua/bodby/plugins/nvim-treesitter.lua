0c1bb7f31e484f9b07c1d0ba0e7b974d1924ff11
Baraa Homsi <baraa.homsi@proton.me>
2025-05-11 09:32:56 +0000

    feat(treesitter): disable indent

--- nvim/lua/bodby/plugins/nvim-treesitter.lua
+++ nvim/lua/bodby/plugins/nvim-treesitter.lua
@@ -13,7 +13,7 @@ return {
     ignore_install = { 'all' },
     highlight = { enable = true },
     incremental_selection = { enable = false },
-    indent = { enable = true },
+    indent = { enable = false },
   },
 
   post = function()
