4038b1bebc35fdee6c5aa3c6a6ee42f9a3298ef1
Baraa Homsi <49784526+bodby@users.noreply.github.com>
2025-05-23 14:57:57 +0000

    style(tree-sitter): spaces for indent

--- nix/tree-sitter.nix
+++ nix/tree-sitter.nix
@@ -47,7 +47,7 @@ fix (self: {
           tree-sitter
         ];
 
-	      # TODO: Do I really need to convert these into strings?
+        # TODO: Do I really need to convert these into strings?
         #       I previously merged an `env` arg with this but it seemed unnecessary.
         env = {
           CFLAGS = lib.escapeShellArgs [
