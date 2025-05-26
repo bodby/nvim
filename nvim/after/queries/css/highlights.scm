749440c23369b913b549b8bbdc8b97a143390f59
Baraa Homsi <baraa.homsi@proton.me>
2025-05-26 18:31:48 +0000

    feat(theme): CSS highlights

--- nvim/after/queries/css/highlights.scm
+++ nvim/after/queries/css/highlights.scm
@@ -1,5 +1,8 @@
 ;; extends
 
-(declaration
-  (property_name)
-    (plain_value) @constant)
+(plain_value) @variable
+
+((plain_value) @constant.builtin
+  (#any-of? @constant.builtin "none" "transparent"))
+
+(unit) @number
